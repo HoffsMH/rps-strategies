95d1cf4b1e52d834777f2246deeef527d40760f7
HoffsMH <hecker.matthew@gmail.com>
2016-04-05 19:38:11 -0600

    adds test file for last strategy

--- lib/strategies/last.rb
+++ lib/strategies/last.rb
@@ -1,3 +1,13 @@
 module Last
-  
+  def self.suggestions
+    
+  end
+
+  def evaluate(game)
+    game.history
+  end
+
+  def self.name
+    'last'
+  end
 end
---
+++ spec/strategies/last_spec.rb
@@ -0,0 +1,53 @@
+require "rspec"
+require "pry"
+
+require_relative "../lib/game"
+require_relative "../lib/strategies/last"
+
+
+describe Last do
+  describe ".evaluate" do
+    context "when given a game with no history" do
+      # human opponents tend to choose rock as their opening move
+      # http://www.livescience.com/15574-win-rock-paper-scissors.html
+      it "returns paper" do
+        game = Game.new
+        result = Last.evaluate(game)
+
+        expect(result).to eq("p")
+      end
+    end
+    context "when given a game with history" do
+      context "and the opponents last move was 'r'" do
+        it "returns p" do
+          game = Game.new
+          game.play({computer: "r", opponent: "r", test_env: true})
+
+          result = Last.evaluate(game)
+
+          expect(result).to eq('p')
+        end
+      end
+      context "and the opponents last move was 'p'" do
+        it "returns s" do
+          game = Game.new
+          game.play({computer: "p", opponent: "p", test_env: true})
+
+          result = Last.evaluate(game)
+
+          expect(result).to eq('s')
+        end
+      end
+      context "and the opponents last move was 's'" do
+        it "returns r" do
+          game = Game.new
+          game.play({computer: "s", opponent: "s", test_env: true})
+
+          result = Last.evaluate(game)
+
+          expect(result).to eq('r')
+        end
+      end
+    end
+  end
+end
